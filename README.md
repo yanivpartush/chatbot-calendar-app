7e93ea0db6719b2c1e8d6c747eede8e2e4b63842
Yaniv P <yanivpartush@gmail.com>
2025-07-31 19:29:04 +0300

    Initial monorepo structure

---
+++ README.md
@@ -0,0 +1,5 @@
+# chatbot-calendar-gateway
+Chatbot calendar gateway
+
+
+docker-compose -p chatbot-calendar-app up
---
+++ chatbot-calendar-gateway/src/chatbot_gateway_app.py
@@ -0,0 +1,62 @@
+from telegram import Update
+from telegram.ext import ApplicationBuilder, MessageHandler, filters, ContextTypes
+from confluent_kafka import Producer
+from dotenv import load_dotenv
+
+import os
+import json
+import logging
+
+load_dotenv() # Load .env file
+
+TOKEN = os.getenv("TOKEN")
+KAFKA_BOOTSTRAP_SERVERS = os.getenv("KAFKA_BOOTSTRAP_SERVERS")
+KAFKA_TOPIC = os.getenv("KAFKA_TOPIC")
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S"
+)
+
+logger = logging.getLogger(__name__)
+
+# Configure Kafka producer
+producer = Producer({'bootstrap.servers': KAFKA_BOOTSTRAP_SERVERS})
+
+def delivery_report(err, msg):
+    """Called once for each message produced to indicate delivery result."""
+    if err is not None:
+        logger.error(f"Message delivery failed: {err}")
+    else:
+        logger.info(f"Message delivered to {msg.topic()} [{msg.partition()}]")
+
+async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    chat_id = update.effective_chat.id
+    user_text = update.message.text
+
+    # 1. Immediate reply to user
+    await context.bot.send_message(chat_id=chat_id, text="Message received! We'll process it soon.")
+
+    # 2. Push the message to Kafka (non-blocking)
+    data = {
+        "chat_id": chat_id,
+        "text": user_text
+    }
+
+    # Serialize as string (or JSON)
+   
+    producer.produce(KAFKA_TOPIC, json.dumps(data).encode('utf-8'), callback=delivery_report)
+    producer.flush(1)
+
+    # The bot does NOT do heavy work here, just queues the message
+
+def main():
+    app = ApplicationBuilder().token(TOKEN).build()
+    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
+
+    logger.info("Bot is running...")
+    app.run_polling()
+
+if __name__ == "__main__":
+    main()
