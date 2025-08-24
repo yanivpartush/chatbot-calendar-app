c7bdb26363e185d92627f7b01c4c3ded0cd0a166
Yaniv P <yanivpartush@gmail.com>
2025-08-24 16:54:28 +0300

    1. Adding prometheus and grafana 2. Adding metrics to UserController 3. Adding keyStorePass key to docker-compose.yaml / service

--- chatbot-calendar-service/src/main/java/com/chatbotcal/controller/UserController.java
+++ chatbot-calendar-service/src/main/java/com/chatbotcal/controller/UserController.java
@@ -2,7 +2,8 @@ package com.chatbotcal.controller;
 
 import com.chatbotcal.controller.dto.UserDtoResponse;
 import com.chatbotcal.service.UserService;
-import lombok.RequiredArgsConstructor;
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,13 +15,27 @@ import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/users")
-@RequiredArgsConstructor
+
 public class UserController {
 
-    private final UserService userService;
+    private UserService userService;
+    private Counter allUsersRequestsCounter;
+    private Counter userByIdRequestsCounter;
+
+    public UserController(UserService userService, MeterRegistry meterRegistry) {
+        this.userService = userService;
+        this.allUsersRequestsCounter = Counter.builder("users_all_requests_total")
+                .description("No. of requests to getAllUsers")
+                .register(meterRegistry);
+
+        this.userByIdRequestsCounter = Counter.builder("users_by_id_requests_total")
+                .description("No. of requests to getUserById")
+                .register(meterRegistry);
+    }
 
     @GetMapping
     public List<UserDtoResponse> getAllUsers() {
+        allUsersRequestsCounter.increment();
         return userService.getAllUsers().stream()
                 .map(UserDtoResponse::fromEntity)
                 .collect(Collectors.toList());
@@ -28,10 +43,10 @@ public class UserController {
 
     @GetMapping("/{id}")
     public ResponseEntity<UserDtoResponse> getUserById(@PathVariable String id) {
+        userByIdRequestsCounter.increment();
         return userService.getUserById(id)
                 .map(UserDtoResponse::fromEntity)
                 .map(ResponseEntity::ok)
                 .orElse(ResponseEntity.notFound().build());
     }
-
 }
